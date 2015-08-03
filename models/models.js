4bb9c2d1107db0a99bfdb22ba09eb8c2371c2e71
dalvarez0 <dalvarez0@users.noreply.github.com>
2015-08-03 03:43:09 +0200

    Despliegue DB en Heroku

---
+++ models/models.js
@@ -0,0 +1,42 @@
+var path = require('path');
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name 	= (url[6] || null);
+var user		= (url[2] || null);
+var pwd		= (url[3] || null);
+var protocol 	= (url[1] || null);
+var dialect 	= (url[1] || null);
+var port		= (url[5] || null);
+var host		= (url[4] || null);
+var storage	= process.env.DATABASE_STORAGE;
+
+//Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+//Usar BD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd, 
+	{dialect: protocol, 
+	 protocol: protocol,
+	 port: port,
+	 storage: storage, //Solo SQLite (.env)
+	 omitNull: true //Solo postgres
+	}
+				);
+
+//Importar la definicion de la tabla quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+exports.Quiz = Quiz; //exportar definición de tabla Quiz
+
+//sequelize.sync() crea e inicializa tabla de preguntas en BD 
+sequelize.sync().success(function() {
+	//success(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().success(function (count){
+		if (count === 0) {
+			Quiz.create ({ pregunta: 'Capital de Italia',
+						respuesta: 'Roma'
+					   })
+			.success(function(){console.log('Base de datos inicializada')});
+		};	
+	});
+});
