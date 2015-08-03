8fd38f1025756d78a5f053ec2f3573cbd32ee901
dalvarez0 <dalvarez0@users.noreply.github.com>
2015-08-03 22:37:12 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -28,7 +28,7 @@ exports.index = function(req, res) {
 
 	} else {
 		models.Quiz.findAll().then(function(quizes) {
-			res.render('quizes/index', {quizes: quizes, errors: []});
+			res.render('quizes/index.ejs', {quizes: quizes, errors: []});
 		}).catch(function(error) {next(error);});
 	}
 
@@ -80,3 +80,28 @@ exports.create = function(req, res){
 };
 
 
+//GET /quizes/:id/edit
+exports.edit = function(req, res){
+	var quiz = req.quiz; //autoload de instancia de quiz
+
+	res.render('quizes/edit', { quiz: quiz, errors: []});
+};
+
+
+//PUT /quizes/:id
+exports.update = function(req, res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz
+	    .validate()
+	    .then(function(err) {
+			if (err) {
+				res.render('quizes/edit', { quiz: req.quiz, errors: err.errors});
+			} else {
+				req.quiz //save: guarda en BD campos pregunta y respuesta de quiz
+				.save({ fields: ["pregunta", "respuesta"]})
+				.then( function(){ res.redirect('/quizes');});
+			} //redirección HTTP a lista de preguntas	
+		});
+};
