ac9dff31008260c9921e4661eb32d1486b9e0bbc
dalvarez0 <dalvarez0@users.noreply.github.com>
2015-08-03 06:44:00 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+//Pagina de entrada
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //Autoload :QuizId
 
 //Definicion de rutas de /quizes
 router.get('/quizes', quizController.index);
